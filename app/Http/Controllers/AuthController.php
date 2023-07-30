28bb70351bfbcd27d7e1511b8af4d4e3357847c4
José Roberto <tekinforroberto@gmail.com>
2023-07-30 12:41:21 -0300

    feat: Implementando endpoint de logout

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -19,5 +19,20 @@ class AuthController extends Controller
         return response_api('Dados retornados com sucesso', $user, 200);
     }
 
+    public function logout(): JsonResponse
+    {
+        try {
+            Auth::user()->currentAccessToken()->delete();
+            return response_api('Logout feito com sucesso', [], 200);
+
+        } catch (\Exception $e) {
+            send_log('Erro ao deslogar usuário', [], 'error', $e);
+            return response_api(
+                'Erro ao deslogar usuário',
+                [],
+                $e->getCode() == 0 ? 500 : $e->getCode()
+            );
+        }
+    }
 
 }
